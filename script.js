02c6bc7aca09001337b2a5b6b491222cd98d2db6
Marina Moroz <moroz2387moroz@yandex.ru>
2024-06-30 15:36:05 +1000

    add script to change background and generate number by click

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ const generateColor = () => {
 const changeBackground = setInterval(() => {
   generateColor();
 }, 300);
+
+const bts = document.querySelector(".generate");
+const num = document.querySelector(".number");
+const generateNumber = () => {
+  const rand = Math.floor(Math.random() * 10 + 1);
+  console.log(rand);
+  num.innerHTML = rand;
+};
+bts.addEventListener("click", generateNumber);
+generateNumber();
